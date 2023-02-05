2cdb6b3c09d512ec36def2e5650ce4efc7862cd2
Simon Vitt <ttivnomis@gmail.com>
2023-02-05 20:30:33 +0100

    added chat

--- djange_chat_app/chat/views.py
+++ djange_chat_app/chat/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        print('Received Data' + request.POST['textmessage'])
     return render(request, 'chat/index.html', {'username': 'Simon'})
